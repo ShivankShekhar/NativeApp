ba813133a7c291755b34c9240f7084355f9224a0
Shivank Shekhar <31924615+ShivankShekhar@users.noreply.github.com>
2019-05-10 11:53:29 +0530

    Add files via upload

--- nativecode/src/app/app.component.ts
+++ nativecode/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { Component, OnInit, ViewChild, ChangeDetectorRef  } from "@angular/core";
 import { NavigationEnd, Router } from "@angular/router";
 import { RouterExtensions } from "nativescript-angular/router";
 import { DrawerTransitionBase, RadSideDrawer, SlideInOnTopTransition } from "nativescript-ui-sidedrawer";
 import { filter } from "rxjs/operators";
 import {User} from "~/app/login/user";
 import {UserDetails} from"~/app/login/user.details"
+import { isAndroid, isIOS } from "tns-core-modules/ui/frame/frame";
+
 import * as app from "tns-core-modules/application";
 
 @Component({
@@ -18,8 +20,8 @@ export class AppComponent implements OnInit {
     private _sideDrawerTransition: DrawerTransitionBase;
     private uName : string="";
     private email : string="";
-
-    constructor(private userDetails:UserDetails,private router: Router, private routerExtensions: RouterExtensions) {
+ private drawer: RadSideDrawer; 
+    constructor(private changeDetectionRef: ChangeDetectorRef,private userDetails:UserDetails,private router: Router, private routerExtensions: RouterExtensions) {
             this.uName =this.userDetails.getUser();
             this.email =this.userDetails.getEmail();
     }
@@ -33,6 +35,16 @@ export class AppComponent implements OnInit {
         .subscribe((event: NavigationEnd) => this._activatedUrl = event.urlAfterRedirects);
         
     }
+
+  onLoaded() {
+       const sideDrawer = <RadSideDrawer>app.getRootView(); 
+    if (isAndroid) {
+      // This disables the swipe gesture to open menu, by setting the treshhold to '0'
+      sideDrawer.android.setTouchTargetThreshold(0);
+    }
+  }
+  
+
 
     get sideDrawerTransition(): DrawerTransitionBase {
         this.uName =this.userDetails.getUser();
